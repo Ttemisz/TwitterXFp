7855df3c2a32a25871aab8d0ddac19840b906b19
Temistoclis <temistoclisxp58@gmail.com>
2024-10-26 01:58:10 -0300

    ajustes

--- twitterxfp/settings.py
+++ twitterxfp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i49-*38dcg+-c&xf84d_n@p+spyx3b0s&hv$(&@gtvdfg-75_%"
+SECRET_KEY = "django-insecure-bi8@d$bprc4d@%$tbu#j7ao94%82xawu1e%z0mkjgse**tkll8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
