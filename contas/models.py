646f89275e78f69533ff80c7a7188215f4617943
Temistoclis <temistoclisxp58@gmail.com>
2024-10-29 11:13:18 -0300

    finalizando

---
+++ contas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Login(models.Model):
+
+    username = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
---
+++ feed/urls.py
@@ -0,0 +1,8 @@
+# urls.py
+from django.urls import path
+from .views import feed_view
+
+urlpatterns = [
+    path('feed/', feed_view, name='feed'),
+    
+]
